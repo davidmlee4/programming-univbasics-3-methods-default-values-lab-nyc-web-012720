187517bb3d6efbc7828b4798bf25b20180c7ab3b
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-16 20:22:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,4 +6,4 @@ def meal_choice(veg1,veg2,protein="meat")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("carrots","broccoli","chicken") 
+meal_choice("carrots","broccoli","chicken")
